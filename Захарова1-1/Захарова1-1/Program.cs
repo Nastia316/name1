a8ec06334df064f974eb4f2d16d0032944ad2f33
Nastia316 <uliazaharova316@gmail.com>
2025-04-23 14:31:55 +0300

    1-1

---
+++ Захарова1-1/Захарова1-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+int n, i, j;
+n = Convert.ToInt32(Console.ReadLine());
+for (i = 2; i < n; i++)
+{
+    j = 2;
+    for (; j <= Math.Sqrt(i); j++)
+    {
+        if (i%j == 0)
+        {
+            break;
+        }
+    }
+    if (j > Math.Sqrt(i)) Console.WriteLine(i);
+}
