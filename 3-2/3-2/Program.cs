43676ca04b49114296c8903ebe92eeda8dd44d03
Nastia316 <uliazaharova316@gmail.com>
2025-04-23 14:37:40 +0300

    3-2

---
+++ 3-2/3-2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напечатать в терминальном окне окружность из символов «*» (при помощи циклов).
+int a = 5;
+for (int i = a; i!=0; i--)
+{
+    string o = "";
+    for (int j = 0; j!=i; j++)
+    {
+        o = o + "  ";
+    }
+    o += "*";
+    for (int k = 0; k != a - i; k++)
+    {
+        o = o + "    ";
+    }
+     o += "*";
+    Console.WriteLine(o);
+}
+for (int i1 = 0; i1 != a+1; i1++)
+{
+    string o = "";
+    for (int j1 = 0; j1 != i1; j1++)
+    {
+        o = o + "  ";
+    }
+    o = o + "*";
+    for (int k1 = 0; k1 != a - i1; k1++)
+    {
+        o = o + "    ";
+    }
+    o += "*";
+    Console.WriteLine(o);
+}
---
+++ 3-2good/3-2good/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+int a = 10;
+
+for (int y = 0; y <= a*2; y++)
+{
+    for (int x = 0; x <= a*2; x++)
+    {
+        double distance = Math.Pow(x - a, 2) + Math.Pow(y - a, 2);
+
+        if (distance >= Math.Pow(a - 0.5, 2) & distance <= Math.Pow(a + 0.5, 2))
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write("  ");
+        }
+    }
+    Console.WriteLine();
+}
